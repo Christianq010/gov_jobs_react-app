73cb584828de8515f71c81abbb45032f27e835e0
Christiaan Quyn <christianq010@gmail.com>
2018-10-16 15:48:14 +0530

    Styling: fix cards on mobile

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -10,6 +10,65 @@ const CardDiv = styled.div`
     height: auto;
     border: 0.8px solid #b6b6b6;
     margin: 1em;
+    .cardbox {
+      display: flex;
+      flex-direction: column-reverse;
+      padding: 2em;
+    }
+    .cardDetails {
+      text-align: center;
+      .headlineText {
+        font-size: 1em;
+        font-weight: 500;
+        margin: 0.3em 0;
+        a {
+          color: #000;
+          text-decoration: none;
+        }
+      }
+      .headlineSub {
+        font-size: 0.8em;
+        font-weight: 400;
+      }
+      .headlineDes {
+        font-size: 1em;
+        margin: 1em 0;
+      }
+      .textRemain {
+        color: #dc2743;
+      }
+    }
+    .cardimgwrapper {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .cardimg {
+        height: 115px;
+        img {
+          height: 100%;
+          border-radius: 50%;
+          background-size: cover;
+        }
+      }
+    }
+    .buttonRow {
+      display: flex;
+      button {
+        color: #fff;
+        font-size: 1em;
+        padding: 0 10px;
+        margin: 1em 1em 0 0;
+        width: 8em;
+        border: 1px solid transparent;
+        line-height: 38px;
+      }
+      .downloadBtn {
+        background: #dc2743;
+      }
+      .viewBtn {
+        background: #d99e40ff;
+      }
+    }
   }
   /* Desktops and laptops ----------- */
   @media only screen and (min-width: 1224px) {
@@ -17,62 +76,62 @@ const CardDiv = styled.div`
     flex-direction: row;
     border: 0.8px solid #b6b6b6;
     margin: 1em;
-  }
-  .cardbox {
-    display: flex;
-    padding: 2.5em;
-  }
-  .cardDetails {
-    .headlineText {
-      font-size: 1em;
-      font-weight: 500;
-      margin: 0.3em 0;
-      a {
-        color: #000;
-        text-decoration: none;
+    .cardbox {
+      display: flex;
+      padding: 2.5em;
+    }
+    .cardDetails {
+      .headlineText {
+        font-size: 1em;
+        font-weight: 500;
+        margin: 0.3em 0;
+        a {
+          color: #000;
+          text-decoration: none;
+        }
+      }
+      .headlineSub {
+        font-size: 0.8em;
+        font-weight: 400;
+      }
+      .headlineDes {
+        font-size: 1em;
+        margin: 1em 0;
+      }
+      .textRemain {
+        color: #dc2743;
       }
     }
-    .headlineSub {
-      font-size: 0.8em;
-      font-weight: 400;
-    }
-    .headlineDes {
-      font-size: 1em;
-      margin: 1em 0;
-    }
-    .textRemain {
-      color: #dc2743;
-    }
-  }
-  .cardimgwrapper {
-    display: flex;
-    align-items: center;
-    padding-left: 4em;
-    .cardimg {
-      height: 115px;
-      img {
-        height: 100%;
-        border-radius: 50%;
-        background-size: cover;
+    .cardimgwrapper {
+      display: flex;
+      align-items: center;
+      padding-left: 4em;
+      .cardimg {
+        height: 115px;
+        img {
+          height: 100%;
+          border-radius: 50%;
+          background-size: cover;
+        }
       }
     }
-  }
-  .buttonRow {
-    display: flex;
-    button {
-      color: #fff;
-      font-size: 1em;
-      padding: 0 10px;
-      margin: 1em 1em 0 0;
-      width: 10em;
-      border: 1px solid transparent;
-      line-height: 38px;
-    }
-    .downloadBtn {
-      background: #DC2743;
-    }
-    .viewBtn {
-      background: #d99e40ff;
+    .buttonRow {
+      display: flex;
+      button {
+        color: #fff;
+        font-size: 1em;
+        padding: 0 10px;
+        margin: 1em 1em 0 0;
+        width: 10em;
+        border: 1px solid transparent;
+        line-height: 38px;
+      }
+      .downloadBtn {
+        background: #dc2743;
+      }
+      .viewBtn {
+        background: #d99e40ff;
+      }
     }
   }
 `;
