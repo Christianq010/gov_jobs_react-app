5a5437688eb1660c257c4713bcd3bfb38e1c2fce
Christiaan Quyn <christianq010@gmail.com>
2018-10-23 17:08:54 +0530

    Feat: add hamburger menu on mobile view

--- src/components/Navbar/Items.js
+++ src/components/Navbar/Items.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Router, Link } from "@reach/router";
 
+import Menu from "./Menu";
+
 const ItemsDiv = styled.div`
   a {
     text-decoration: none;
@@ -9,7 +11,9 @@ const ItemsDiv = styled.div`
   }
   /* Smartphones (portrait and landscape) ----------- */
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    display: none;
+    .navlink {
+      display: none;
+    }
   }
   /* Desktops and laptops ----------- */
   @media only screen and (min-width: 1224px) {
@@ -29,6 +33,9 @@ const ItemsDiv = styled.div`
       padding: 0.3em 1em;
       background: #d99e40ff;
       border: 1px solid transparent;
+    }
+    .burgerMenu {
+      display: none;
     }
   }
 `;
@@ -56,6 +63,9 @@ class Items extends React.Component {
           <Link className="" to="/">
             <div className="linkbtn">Get Hired</div>
           </Link>
+        </div>
+        <div className="burgerMenu">
+          <Menu />
         </div>
       </ItemsDiv>
     );
