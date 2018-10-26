52bf8f1fe7ce8bfe3c6e62fe844d58d02f10312a
Christiaan Quyn <christianq010@gmail.com>
2018-10-26 13:45:35 +0530

    Feat: print response of StoryBlok onto screen

--- src/components/Cards/CardWrapper.js
+++ src/components/Cards/CardWrapper.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+import axios from 'axios'
 import Card from "./Card";
+
+const API =
+  "https://api.storyblok.com/v1/cdn/stories?token=lClgQFxlCzg1pp3obj6aUQtt&cv=1540525479";
 
 const CardWrapperDiv = styled.div`
   /* Smartphones (portrait and landscape) ----------- */
@@ -19,8 +23,8 @@ const CardWrapperDiv = styled.div`
 `;
 
 const Cards = styled.div`
-    /* Smartphones (portrait and landscape) ----------- */
-    @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  /* Smartphones (portrait and landscape) ----------- */
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
     display: flex;
     flex-direction: column;
   }
@@ -32,14 +36,26 @@ const Cards = styled.div`
 `;
 
 class CardWrapper extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      stories: [],
+    }
+  }
+  componentDidMount() {
+    axios.get(API)
+      // .then(response => console.log(response))
+      .then(data => {this.setState({ stories: data.data.stories})});
+  }
+
   render() {
     return (
       <CardWrapperDiv>
         <Cards>
           <Card />
-          <Card />
-          <Card />
-          <Card />
+          <pre>
+            <code>{JSON.stringify(this.state, null, 2)}</code>
+          </pre>
         </Cards>
       </CardWrapperDiv>
     );
